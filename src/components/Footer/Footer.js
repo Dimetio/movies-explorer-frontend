36c6012c1f8717f935518f1e679664938df7eae2
ddubinin <dubinin.dmitry@mail.ru>
2022-08-03 19:03:13 +0300

    feat: added component Footer

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,25 @@
+import './Footer.css';
+
+export default function Footer() {
+  return (
+    <section className="footer">
+      <h3 className="footer__title">Учебный проект Яндекс.Практикум х BeatFilm.</h3>
+
+      <div className="footer-wrap">
+        <span className="footer__date">© 2020</span>
+
+        <ul className="footer__social-links">
+            <li className="footer__social-links__item">
+                <a href="https://practicum.yandex.ru/" target="_blank" className="footer__social-links__link" rel="noreferrer">Яндекс.Практикум</a>
+            </li>
+            <li className="footer__social-links__item">
+                <a href="https://github.com/Dimetio" target="_blank" className="footer__social-links__link" rel="noreferrer">Github</a>
+            </li>
+            <li className="footer__social-links__item">
+                <a href="https://www.facebook.com/profile.php?id=100008160217923" target="_blank" className="footer__social-links__link" rel="noreferrer">Facebook</a>
+            </li>
+          </ul>
+      </div>
+    </section>
+  )
+}
