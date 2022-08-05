7ce986e16076e9c365df484b8471d56d946b934b
ddubinin <dubinin.dmitry@mail.ru>
2022-08-05 17:58:36 +0300

    feat: added component SearchForm

---
+++ src/components/SearchForm/SearchForm.js
@@ -0,0 +1,28 @@
+import './SearchForm.css';
+import icon from '../../images/search.svg';
+
+export default function SearchForm() {
+  return (
+    <form className="search-form container">
+        <div className="search-from__wrap">
+          <button type="submit" className="search-form__button search-form__button_primary">
+            <img src={icon} className="search-form__icon" alt="иконка"/>
+          </button>
+
+          <input type="text" className="search-form__input" placeholder="Фильм"/>
+
+          <button type="submit" className="search-form__button search-form__button_secondary">
+            <img src={icon} className="search-form__icon" alt="иконка"/>
+          </button>
+        </div>
+
+        <div className="switch-wrap">
+          <label className="switch">
+            <input type="checkbox" className="switch__input"/>
+            <span className="switch__slider round"></span>
+          </label>
+          <p className="switch-text">Короткометражки</p>
+        </div>
+    </form>
+  )
+}
