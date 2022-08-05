3a0893241fd957692b145abb0a915f5045de3e4d
ddubinin <dubinin.dmitry@mail.ru>
2022-08-05 23:27:42 +0300

    feat: update moviesCard

--- src/components/MoviesCard/MoviesCard.js
+++ src/components/MoviesCard/MoviesCard.js
@@ -2,15 +2,24 @@ import './MoviesCard.css';
 import cardPhoto from '../../images/phoro-1.jpg';
 import like from '../../images/like.svg';
 import likeActive from '../../images/like-active.svg';
+import deleteIcon from '../../images/delete.svg';
 
 export default function MoviesCard() {
+  let isMyCard;
+
+  window.location.pathname === '/saved-movies' 
+  ? isMyCard = true 
+  : isMyCard = false;
+
   return (
     <div className="card">
       <img src={cardPhoto} alt="картинка" className="card__img"/>
 
       <div className="card-wrap">
         <p className="card__title">33 слова о дизайне</p>
-        <img src={likeActive} alt="сердечко" className="card__like"/>
+
+        {isMyCard && <img src={deleteIcon} alt="сердечко" className="card-icon card__delete"/>}
+        {!isMyCard && <img src={likeActive} alt="сердечко" className="card-icon card__like"/>}
       </div>
 
       <span className="card__duration">1ч 47м</span>
