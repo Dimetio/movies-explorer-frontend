3e3be8637797fc3172858c579ea3ef493f7b53a6
ddubinin <dubinin.dmitry@mail.ru>
2022-08-16 12:42:02 +0300

    feat: added component preloader

--- src/components/Movies/Movies.js
+++ src/components/Movies/Movies.js
@@ -1,12 +1,13 @@
 import './Movies.css';
 import SearchForm from '../SearchForm/SearchForm';
 import MoviesCardList from '../MoviesCardList/MoviesCardList';
+import Preloader from '../Preloader/Preloader';
 
 export default function Movies() {
   return (
     <>
       <SearchForm />
-
+      <Preloader />
       <MoviesCardList />
 
       <button type="button" className="movies-list__more">Ещё</button>
