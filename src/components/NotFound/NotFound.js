529e9e07d903fce67548e9d483dc5802cd329058
ddubinin <dubinin.dmitry@mail.ru>
2022-08-21 12:33:13 +0300

    feat: set up go back

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,12 +1,22 @@
+import { useNavigate } from "react-router-dom";
 import './NotFound.css'
 
 export default function NotFound() {
+  const navigate = useNavigate();
+
+  function handleClick() {
+    return navigate(-1);
+  }
+
   return (
     <section className="not-found">
       <p className="not-found__code">404</p>
       <p className="not-found__text">Страница не найдена</p>
 
-      <a href="/" className="not-found__link">Назад</a>
+      <button
+        className="not-found__back"
+        onClick={handleClick}
+      >Назад</button>
     </section>
   )
 }
