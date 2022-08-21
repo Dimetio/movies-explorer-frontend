243a695e97f10a0c08677894240919a6beff7a90
ddubinin <dubinin.dmitry@mail.ru>
2022-08-21 12:19:45 +0300

    feat: protectedRoute

---
+++ src/components/ProtectedRoute/ProtectedRoute.js
@@ -0,0 +1,9 @@
+import { Navigate } from "react-router-dom";
+
+export default function ProtectedRoute({isLoggedIn, children}) {
+  return (
+    <>
+      {isLoggedIn ? children : <Navigate to="/signin" />}
+    </>
+  )
+}
