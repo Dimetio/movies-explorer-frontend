5b1b82191b6f80060cd31dd98316d1ee890dd85f
ddubinin <dubinin.dmitry@mail.ru>
2022-08-24 15:21:04 +0300

    feat: update movies card list

--- src/components/MoviesCardList/MoviesCardList.js
+++ src/components/MoviesCardList/MoviesCardList.js
@@ -1,17 +1,24 @@
 import './MoviesCardList.css';
 import MoviesCard from '../MoviesCard/MoviesCard';
 
-export default function MoviesCardList() {
+export default function MoviesCardList({
+  movies, 
+  handleMovieIconClick,
+  isLike,
+  moviesListLength,
+}) {
   return (
     <section className="movies-list">
-      <MoviesCard />
-      <MoviesCard />
-      <MoviesCard />
-      <MoviesCard />
-      <MoviesCard />
-      <MoviesCard />
-      <MoviesCard />
-      <MoviesCard />
+      {
+        movies.map((movie, index) => (
+          <MoviesCard 
+            movie={movie}
+            key={movie.id ? movie.id : index}
+            handleMovieIconClick={handleMovieIconClick}
+            isLike={isLike}
+          />
+        )).slice(0, moviesListLength)
+      }
     </section>
   )
 }
